e227ee8baf1301a9317fb1ec3cc1a11d961af820
AlissonOliveira-Jar <alisson.dev.py@gmail.com>
2025-03-28 17:44:15 -0300

    feature: todo controller

---
+++ src/main/java/com/desafio/todo/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.desafio.todo.controller;
+
+import com.desafio.todo.entity.Todo;
+import com.desafio.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(Todo todo) {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") UUID ID) {
+        return todoService.delete(ID);
+    }
+}
